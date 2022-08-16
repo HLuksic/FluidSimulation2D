aac846a9fcc4ab79d1e6c0b37d2d65309578f823
HLuksic <hluksic20foi.hr>
2022-08-16 14:53:19 +0200

    Final bugfixes

--- FluidSimulation2D/main.cpp
+++ FluidSimulation2D/main.cpp
@@ -6,25 +6,24 @@
 constexpr int N = 100;
 constexpr int iter = 8;
 
-constexpr auto Get2DCoordinate(int x, int y) 
+constexpr int Get2DCoordinate(int x, int y) 
 {
-    if (x < 0) { x = 0; }
-    if (x > N - 1) { x = N - 1; }
-
-    if (y < 0) { y = 0; }
-    if (y > N - 1) { y = N - 1; }
+    if (x < 0) x = 0;
+    if (y < 0) y = 0;
+    if (x > N - 1) x = N - 1;
+    if (y > N - 1) y = N - 1;
 
     return (y * N) + x;
 }
 
-class FluidGrid 
+class Fluid 
 {
 public:
-    FluidGrid(float dt, float d, float v) 
+    Fluid(float dt, float d, float v) 
     {
-        timeStep = dt;
+        timeStep     = dt;
         diffusionAmt = d;
-        viscosity = v;
+        viscosity    = v;
     }
 
 public:
@@ -58,7 +57,7 @@ public:
             for (int j = 0; j < N; j++)
             {
                 if (density[Get2DCoordinate(i, j)] > 0.0f)
-                    density[Get2DCoordinate(i, j)] -= 3.0f * fElapsedTime;
+                    density[Get2DCoordinate(i, j)] -= 1.0f * fElapsedTime;
                 if (density[Get2DCoordinate(i, j)] > 254.9f)
                     density[Get2DCoordinate(i, j)] == 254.9f;
             }
@@ -77,9 +76,9 @@ public:
         velocityY[index] += amountY;
     }
 
-    void Diffuse(int b, float x[], float x0[], float diffusion, float timeStep) 
+    void Diffuse(int b, float x[], float x0[], float factor, float timeStep) 
     {
-        float a = timeStep * diffusion * (N - 2) * (N - 2);
+        float a = timeStep * factor * (N - 2) * (N - 2);
         this->LinearSolve(b, x, x0, a, 1 + 6 * a);
     }
 
@@ -101,29 +100,27 @@ public:
         }
     }
 
-    void Project(float velocX[], float velocY[], float p[], float div[]) 
+    void Project(float velocX[], float velocY[], float velocX0[], float velocY0[]) 
     {
         for (int j = 1; j < N - 1; j++)
             for (int i = 1; i < N - 1; i++) 
             {
-                div[Get2DCoordinate(i, j)] = -0.5f * 
-                     (velocX[Get2DCoordinate(i + 1, j)]
-                    - velocX[Get2DCoordinate(i - 1, j)]
-                    + velocY[Get2DCoordinate(i, j + 1)]
-                    - velocY[Get2DCoordinate(i, j - 1)]) / N;
-
-                p[Get2DCoordinate(i, j)] = 0;
+                velocY0[Get2DCoordinate(i, j)] = -0.5f * (velocX[Get2DCoordinate(i + 1, j)]
+                                                        - velocX[Get2DCoordinate(i - 1, j)]
+                                                        + velocY[Get2DCoordinate(i, j + 1)]
+                                                        - velocY[Get2DCoordinate(i, j - 1)]) / N;
+                velocX0[Get2DCoordinate(i, j)] = 0;
             }
 
-        this->SetBoundary(0, div);
-        this->SetBoundary(0, p);
-        this->LinearSolve(0, p, div, 1, 6);
+        this->SetBoundary(0, velocY0);
+        this->SetBoundary(0, velocX0);
+        this->LinearSolve(0, velocX0, velocY0, 1, 6);
 
         for (int j = 1; j < N - 1; j++)
             for (int i = 1; i < N - 1; i++) 
             {
-                velocX[Get2DCoordinate(i, j)] -= 0.5f * (p[Get2DCoordinate(i + 1, j)] - p[Get2DCoordinate(i - 1, j)]) * N;
-                velocY[Get2DCoordinate(i, j)] -= 0.5f * (p[Get2DCoordinate(i, j + 1)] - p[Get2DCoordinate(i, j - 1)]) * N;
+                velocX[Get2DCoordinate(i, j)] -= 0.5f * (velocX0[Get2DCoordinate(i + 1, j)] - velocX0[Get2DCoordinate(i - 1, j)]) * N;
+                velocY[Get2DCoordinate(i, j)] -= 0.5f * (velocX0[Get2DCoordinate(i, j + 1)] - velocX0[Get2DCoordinate(i, j - 1)]) * N;
             }
 
         this->SetBoundary(1, velocX);
@@ -133,39 +130,27 @@ public:
 
     void Advect(int b, float d[], float d0[], float velocX[], float velocY[], float timeStep)
     {
-        float i0, i1, j0, j1;
+        int i0, i1, j0, j1;
+        float s0, s1, t0, t1, x, y, timeStep0;
 
-        float timeStepX = timeStep * (N - 2);
-        float timeStepY = timeStep * (N - 2);
-
-        float s0, s1, t0, t1;
-        float tmp1, tmp2, x, y;
+        timeStep0 = timeStep * (N - 2);
 
         for (int j = 1; j < N - 1; j++) 
             for (int i = 1; i < N - 1; i++) 
             {
-                tmp1 = timeStepX * velocX[Get2DCoordinate(i, j)];
-                tmp2 = timeStepY * velocY[Get2DCoordinate(i, j)];
-                x = float(i) - tmp1;
-                y = float(j) - tmp2;
+                x = float(i) - timeStep0 * velocX[Get2DCoordinate(i, j)];
+                y = float(j) - timeStep0 * velocY[Get2DCoordinate(i, j)];
 
-                if (x < 0.5f) 
-                    x = 0.5f;
+                if (x < 0.5f) x = 0.5f;
+                if (y < 0.5f) y = 0.5f;
+                if (x > float(N) + 0.5f) x = float(N) + 0.5f;
+                if (y > float(N) + 0.5f) y = float(N) + 0.5f;
 
-                if (x > float(N) + 0.5f)
-                    x = float(N) + 0.5f;
+                i0 = int(x);
+                i1 = i0 + 1;
 
-                i0 = floorf(x);
-                i1 = i0 + 1.0f;
-
-                if (y < 0.5f) 
-                    y = 0.5f;
-
-                if (y > float(N) + 0.5f)
-                    y = float(N) + 0.5f;
-
-                j0 = floorf(y);
-                j1 = j0 + 1.0f;
+                j0 = int(y);
+                j1 = j0 + 1;
 
                 s1 = x - i0;
                 s0 = 1.0f - s1;
@@ -175,16 +160,16 @@ public:
 
                 d[Get2DCoordinate(i, j)] =
                     s0 * (t0 * d0[Get2DCoordinate(int(i0), int(j0))] + t1 * d0[Get2DCoordinate(int(i0), int(j1))]) +
-                    s1 * (t0 * d0[Get2DCoordinate(int(i1), int(j1))] + t1 * d0[Get2DCoordinate(int(i1), int(j1))]);
+                    s1 * (t0 * d0[Get2DCoordinate(int(i1), int(j0))] + t1 * d0[Get2DCoordinate(int(i1), int(j1))]);
             }
 
-        SetBoundary(b, d);
+        this->SetBoundary(b, d);
     }
 
 
     void SetBoundary(int b, float x[]) 
     {
-        /*for (int i = 1; i < N - 1; i++) 
+        for (int i = 1; i < N - 1; i++) 
         {
             x[Get2DCoordinate(i, 0)]     = b == 2 ? -x[Get2DCoordinate(i, 1)]     : x[Get2DCoordinate(i, 1)];
             x[Get2DCoordinate(i, N - 1)] = b == 2 ? -x[Get2DCoordinate(i, N - 2)] : x[Get2DCoordinate(i, N - 2)];
@@ -192,38 +177,10 @@ public:
             x[Get2DCoordinate(N - 1, i)] = b == 1 ? -x[Get2DCoordinate(N - 2, i)] : x[Get2DCoordinate(N - 2, i)];
         }
 
-        x[Get2DCoordinate(0, 0)] = 0.33f *
-            (x[Get2DCoordinate(1, 0)]
-           + x[Get2DCoordinate(0, 1)]
-           + x[Get2DCoordinate(0, 0)]);
-
-        x[Get2DCoordinate(0, N - 1)] = 0.33f * 
-             (x[Get2DCoordinate(0, N - 2)]
-            + x[Get2DCoordinate(1, N - 1)]
-            + x[Get2DCoordinate(0, N - 1)]);
-
-        x[Get2DCoordinate(N - 1, 0)] = 0.33f * 
-             (x[Get2DCoordinate(N - 2, 0)]
-            + x[Get2DCoordinate(N - 1, 1)]
-            + x[Get2DCoordinate(N - 1, 0)]);
-
-        x[Get2DCoordinate(N - 1, N - 1)] = 0.33f * 
-             (x[Get2DCoordinate(N - 1, N - 2)]
-            + x[Get2DCoordinate(N - 2, N - 1)]
-            + x[Get2DCoordinate(N - 1, N - 1)]);*/
-
-        for (int i = 1; i <= N; i++)
-        {
-            x[Get2DCoordinate(0, i)]     = b == 1 ? -x[Get2DCoordinate(1, i)] : x[Get2DCoordinate(1, i)];
-            x[Get2DCoordinate(N + 1, i)] = b == 1 ? -x[Get2DCoordinate(N, i)] : x[Get2DCoordinate(N, i)];
-            x[Get2DCoordinate(i, 0)]     = b == 2 ? -x[Get2DCoordinate(i, 1)] : x[Get2DCoordinate(i, 1)];
-            x[Get2DCoordinate(i, N + 1)] = b == 2 ? -x[Get2DCoordinate(i, N)] : x[Get2DCoordinate(i, N)];
-        }
-
-        x[Get2DCoordinate(0, 0)]         = 0.5f * (x[Get2DCoordinate(1, 0)]     + x[Get2DCoordinate(0, 1)]);
-        x[Get2DCoordinate(0, N + 1)]     = 0.5f * (x[Get2DCoordinate(1, N + 1)] + x[Get2DCoordinate(0, N)]);
-        x[Get2DCoordinate(N + 1, 0)]     = 0.5f * (x[Get2DCoordinate(N, 0)]     + x[Get2DCoordinate(N + 1, 1)]);
-        x[Get2DCoordinate(N + 1, N + 1)] = 0.5f * (x[Get2DCoordinate(N, N + 1)] + x[Get2DCoordinate(N + 1, N)]);
+        x[Get2DCoordinate(0, 0)]         = 0.33f * (x[Get2DCoordinate(1, 0)]         + x[Get2DCoordinate(0, 1)]         + x[Get2DCoordinate(0, 0)]);
+        x[Get2DCoordinate(0, N - 1)]     = 0.33f * (x[Get2DCoordinate(0, N - 2)]     + x[Get2DCoordinate(1, N - 1)]     + x[Get2DCoordinate(0, N - 1)]);
+        x[Get2DCoordinate(N - 1, 0)]     = 0.33f * (x[Get2DCoordinate(N - 2, 0)]     + x[Get2DCoordinate(N - 1, 1)]     + x[Get2DCoordinate(N - 1, 0)]);
+        x[Get2DCoordinate(N - 1, N - 1)] = 0.33f * (x[Get2DCoordinate(N - 1, N - 2)] + x[Get2DCoordinate(N - 2, N - 1)] + x[Get2DCoordinate(N - 1, N - 1)]);
     }
 };
 
@@ -236,7 +193,7 @@ public:
 	}
 
 private:
-    FluidGrid* Fluid;
+    Fluid* _Fluid;
     olc::vi2d mousePos;
     olc::vi2d pMousePos;
 
@@ -251,15 +208,38 @@ private:
 
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
-                Draw({ i , j }, olc::Pixel(255, 255, 255, uint8_t(Fluid->density[Get2DCoordinate(i, j)])));
+                Draw({ i , j }, olc::Pixel(255, 255, 255, uint8_t(_Fluid->density[Get2DCoordinate(i, j)])));
 
         DrawRect({ 0,0 }, { ScreenWidth() - 1, ScreenHeight() - 1 }, olc::VERY_DARK_BLUE);
+    }
+
+    void Input()
+    {
+        mousePos = GetMousePos();
+
+        if (GetMouse(olc::Mouse::LEFT).bHeld)
+        {
+            _Fluid->AddDensity(GetMouseX(), GetMouseY(), 250.0f);
+            _Fluid->AddVelocity(GetMouseX(), GetMouseY(), (mousePos.x - pMousePos.x) / 4.0f, (mousePos.y - pMousePos.y) / 4.0f);
+        }
+
+        pMousePos = mousePos;
+
+        if (GetKey(olc::Key::R).bPressed)
+        {
+            memset(_Fluid->density,    0, sizeof(_Fluid->density));
+            memset(_Fluid->pDensity,   0, sizeof(_Fluid->pDensity));
+            memset(_Fluid->velocityX,  0, sizeof(_Fluid->velocityX));
+            memset(_Fluid->velocityY,  0, sizeof(_Fluid->velocityY));
+            memset(_Fluid->pVelocityX, 0, sizeof(_Fluid->pVelocityX));
+            memset(_Fluid->pVelocityY, 0, sizeof(_Fluid->pVelocityY));
+        }
     }
 
 public:
 	bool OnUserCreate() override
 	{
-        Fluid = new FluidGrid(0.5f, 0.00001f, 0.00001f);
+        _Fluid = new Fluid(0.5f, 0.000001f, 0.0000001f);
         mousePos = GetMousePos();
         pMousePos = GetMousePos();
         SetPixelMode(olc::Pixel::ALPHA);
@@ -269,27 +249,8 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-        mousePos = GetMousePos();
-
-        if (GetMouse(olc::Mouse::LEFT).bHeld)
-        {
-            Fluid->AddDensity(GetMouseX(), GetMouseY(), 250.0f);
-            Fluid->AddVelocity(GetMouseX(), GetMouseY(), 0.3f, 0.0f/*(mousePos.x - pMousePos.x) / 3.0f, (mousePos.y - pMousePos.y) / 3.0f*/);
-        }
-
-        pMousePos = mousePos;
-
-        if (GetKey(olc::Key::R).bPressed)
-        {
-            memset(Fluid->density,    0, sizeof(Fluid->density));
-            memset(Fluid->pDensity,   0, sizeof(Fluid->pDensity));
-            memset(Fluid->velocityX,  0, sizeof(Fluid->velocityX));
-            memset(Fluid->velocityY,  0, sizeof(Fluid->velocityY));
-            memset(Fluid->pVelocityX, 0, sizeof(Fluid->pVelocityX));
-            memset(Fluid->pVelocityY, 0, sizeof(Fluid->pVelocityY));
-        }
-
-        Fluid->Step(fElapsedTime);
+        _Fluid->Step(fElapsedTime);
+        Input();
         Render();
 
         return true;
